5580141f2753db5a95f8607edb846f97937998fd
Bhupinder Singh <bhupi98.bs@gmail.com>
2023-09-30 16:10:05 -0700

    done

--- Lab3-Modules,testing and trigonometry/sphericalToRectangular.cpp
+++ Lab3-Modules,testing and trigonometry/sphericalToRectangular.cpp
@@ -12,7 +12,12 @@
 double *sphericalToRectangular(double r, double azimuth, double inclination) {
     double x = r * sin(inclination) * cos(azimuth);
     double y = r * sin(inclination) * sin(azimuth);
-    double z = r * sin(inclination);
+    double z = r * cos(inclination);
+    if (inclination == 0) { // work with 2d points instead
+        x = r * cos(azimuth);
+        y = r * sin(azimuth);
+        z = 0;
+    }
     double *cartesianArray = (double*) malloc(3 * sizeof (double));
     cartesianArray[0] = x;
     cartesianArray[1] = y;
@@ -29,10 +34,25 @@ SphericalCoords rectangularToSpherical(double x, double y, double z) {// doesn't
         sphericalCoordinates.inclination = atan(sqrt(x * x + y * y) / z);
     } else if (z < 0) {
         sphericalCoordinates.inclination = M_PI + atan(sqrt(x * x + y * y) / z);
-    } else if (z = 0 && x * y != 0) {
-        sphericalCoordinates.inclination = M_PI;
+    } else if (z == 0 && x  != 0 && y != 0) {
+        sphericalCoordinates.inclination = M_PI/2;
     } else if (x == 0 && y == 0 && z == 0) {
         sphericalCoordinates.inclination = NULL;
+    }
+
+    if (x > 0) {
+        sphericalCoordinates.azimuth = atan(y/x);
+    } else if (x < 0 && y >= 0) {
+        sphericalCoordinates.azimuth = atan(y/x) + M_PI;
+    } else if (x < 0 && y < 0) {
+        sphericalCoordinates.azimuth = atan(y/x) - M_PI;
+    } else if (x == 0 && y > 0) {
+        sphericalCoordinates.azimuth = M_PI;
+    } else if (x == 0 && y < 0) {
+        sphericalCoordinates.azimuth = -M_PI;
+    }
+    if (x == 0 && y == 0) {
+        sphericalCoordinates.azimuth = NULL;
     }
     return sphericalCoordinates;
 
